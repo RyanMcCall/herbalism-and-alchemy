ab78df36a91f874f1e4510ba5854f5891b41ce66
Ryan McCall <ryan.e.mccall@gmail.com>
2020-08-24 09:33:06 -0500

    Completed .describe for ingredients

---
+++ ingredients.py
@@ -0,0 +1,16 @@
+class Ingredient():
+    def __init__(self, name, rarity, details, DC, regions, description):
+        self.name = name
+        self.rarity = rarity
+        self.details = details
+        self.DC = DC
+        self.regions = regions
+        self.description = description
+
+    def describe(self):
+        print(f'Ingredient: {self.name}, 
+                rarity: {self.rarity}, 
+                details: {self.details}, 
+                DC: {self.DC}, 
+                regions: {self.regions}, 
+                description: {self.description})
