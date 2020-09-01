e2e7c05e46cc19988fddf509db7d1e94fd44cdd2
Ryan McCall <ryan.e.mccall@gmail.com>
2020-09-01 09:11:57 -0500

    Declare run and start roll_table creation.

---
+++ region_roll_tables.py
@@ -0,0 +1,19 @@
+def run():
+    print('Rolling ingredients...')
+    print(roll_tables['arctic'][3])
+
+roll_tables = {
+    'arctic': {
+        2: 'Silver Hibiscus',
+        3: 'Mortflesh Powder',
+        4: 'Ironwood Heart',
+        5: 'Frozen Seedlings',
+        6: 'Common Ingredient',
+        7: 'Common Ingredient',
+        8: 'Common Ingredient',
+        9: 'Arctic Creeper',
+        10: 'Fennel Silk',
+        11: "Fiend's Ivy",
+        12: 'Voidroot'
+    }
+}
