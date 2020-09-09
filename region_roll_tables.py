1400c18a7cd3a32ba0b7f1ad263236b92622ae62
Ryan McCall <ryan.e.mccall@gmail.com>
2020-09-09 10:32:28 -0500

    Add formatting

--- region_roll_tables.py
+++ region_roll_tables.py
@@ -1,25 +1,25 @@
 def run():
-    print('Rolling ingredients...')
+    print("Rolling ingredients...")
+
 
 roll_tables = {
-    'arctic': {
-        2: 'Silver Hibiscus',
-        3: 'Mortflesh Powder',
-        4: 'Ironwood Heart',
-        5: 'Frozen Seedlings',
-        6: 'Common Ingredient',
-        7: 'Common Ingredient',
-        8: 'Common Ingredient',
-        9: 'Arctic Creeper',
-        10: 'Fennel Silk',
+    "arctic": {
+        2: "Silver Hibiscus",
+        3: "Mortflesh Powder",
+        4: "Ironwood Heart",
+        5: "Frozen Seedlings",
+        6: "Common Ingredient",
+        7: "Common Ingredient",
+        8: "Common Ingredient",
+        9: "Arctic Creeper",
+        10: "Fennel Silk",
         11: "Fiend's Ivy",
-        12: 'Voidroot'
+        12: "Voidroot",
     },
-    'coastal': {
-        2: 'Hydrathistle',
-        3: 'Amanita Cap',
-        4: 'Hyancinth Nectar',
-        5: 'Chromus Slime',
-
-    }
+    "coastal": {
+        2: "Hydrathistle",
+        3: "Amanita Cap",
+        4: "Hyancinth Nectar",
+        5: "Chromus Slime",
+    },
 }
